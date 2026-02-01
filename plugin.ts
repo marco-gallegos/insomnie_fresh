7a9b98bc2fb6e94a4845458da81871c7f5e88d49
Marco A. Gallegos <ma_galeza@hotmail.com>
2026-01-31 22:43:48 -0600

    updated according example and not init

--- plugin.ts
+++ plugin.ts
@@ -3,25 +3,17 @@
 // Documentation: https://github.com/user/fresh/blob/main/docs/plugins.md
 const editor = getEditor();
 
-
-// Register a command that users can invoke
-editor.registerCommand("bitch", "Say bitch", async () => {
+globalThis.call_api = function() {
+  console.log("calling api");
   editor.showStatusMessage("Hello from your plugin bitch!");
-});
+
+}
 
 // React to editor events
-editor.on("buffer_opened", (event) => {
-  const info = editor.getBufferInfo();
-  console.log("opened")
-  editor.debug("okokokokokoko")
-  if (info) {
-    console.log(`Opened: ${info.path}`);
-  }
+editor.on("cursor_moved", (event) => {
+  editor.showStatusMessage("jsjsjsjsjsjsjsjsjsjsj (risa dev)")
 });
 
-// Example: Add a keybinding in your Fresh config:
-// {
-//   "keyBindings": {
-//     "ctrl+alt+h": "command:hello"
-//   }
-// }
+// Register a command that users can invoke
+editor.registerCommand("call_api", "Call API","call_api", null);
+
